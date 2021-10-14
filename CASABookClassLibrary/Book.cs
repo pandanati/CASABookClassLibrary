282e8a23afbeed9f7ef06a57f1efa02418c8980d
pandanati <72080264+pandanati@users.noreply.github.com>
2021-10-14 20:45:28 +0200

    Add project files.

---
+++ CASABookClassLibrary/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CASABookClassLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public string ISBN { get; set; }
+    }
+}
